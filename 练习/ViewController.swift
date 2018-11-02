e38d2613668d489dd3f45e1ff63e55f588e71354
jkgaoyuan <jkgaoyuan@gmai.com>
2018-11-03 00:56:50 +0800

    Initial Commit

---
+++ 练习/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  练习
+//
+//  Created by 高渊 on 2018/11/3.
+//  Copyright © 2018年 高渊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
